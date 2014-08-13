aa6f8456bea37becad9e630b9e5cff9b19714f2d
Kyle Smith & Brandon Madsen <student@epicodus.com>
2014-08-13 11:15:02 -0700

    initialize a list with a name

---
+++ lib/list.rb
@@ -0,0 +1,5 @@
+class List
+  def initialize(name)
+
+  end
+end
---
+++ spec/list_spec.rb
@@ -0,0 +1,17 @@
+require 'list'
+require 'pg'
+
+DB = PG.connect(:dbname => 'to_do_test')
+
+RSpec.configure do |config|
+  config.after(:each)do
+  DB.exec("DELETE FROM lists *;")
+  end
+end
+
+describe List do
+  it 'is initialized with a name' do
+    list = List.new("Epicodus stuff")
+    expect(list).to be_an_instance_of List
+  end
+end
